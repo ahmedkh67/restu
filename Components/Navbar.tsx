09e6e5e8936b19a0a10ce7b8dd96bee0c0b6e5b4
ahmedkh67 <al.monster.games@gmail.com>
2025-01-06 10:44:40 +0300

    h

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -3,10 +3,12 @@ import logo from '@/public/MenuIcon.svg'
 import Image from 'next/image'
 import { Logoname } from '@/Constants'
 
+//md:w-[1720px] md:h-[84px] w-[355px] h-[70px]
+
 const Navbar = () => {
   return (
     <>
-    <div className="flex md:w-[1720px] md:h-[84px] w-[355px] h-[70px] justify-between items-center px-4
+    <div className="flex max-w-6/7 min-w-80 h-20 justify-between items-center px-4
       rounded-3xl md:px-6 py-10 my-8 Nav_Background">
       <h1 className="text-6xl">
         {Logoname}
