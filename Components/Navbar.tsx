242eebfcd08554607f2387c12610a73f864f6050
ahmedkh67 <al.monster.games@gmail.com>
2025-01-06 09:40:02 +0300

    th

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -7,10 +7,9 @@ const Navbar = () => {
   return (
     <>
     <div className="flex md:w-[1720px] md:h-[84px] w-[355px] h-[70px] justify-between items-center px-4
-      rounded-3xl md:px-6 py-10 my-8 Nav_Background">
+      rounded-3xl md:px-6 py-10 my-8 bg-[#ffffff28]">
       <h1 className="text-6xl">
         {Logoname}
-
       </h1>
       <Image src={logo} alt={'logoimag'} width={70} height={100} className='Menu_Icon' />
 
