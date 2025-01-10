d86eb606f6893c018aef31df04b50a0117142e75
ahmedkh67 <al.monster.games@gmail.com>
2025-01-10 17:40:04 +0300

    gh

--- Components/Distancesec.tsx
+++ Components/Distancesec.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Distancesec = () => {
   return (
     <div className="flex w-full h-70 Distance_Background1 justify-center items-center">
-        <div className='md:text-7xl text-4xl text-white opacity-80 border-26 border-double p-4 text-center'>
+        <div className='md:text-7xl text-4xl text-white opacity-80 border-9 border-solid p-4 text-center'>
             Dina Resturante
         </div>
     </div>
