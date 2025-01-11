9d359932e8787f078c24b97c5f512da86abaae0f
ahmedkh67 <al.monster.games@gmail.com>
2025-01-11 13:44:25 +0300

    tyuty

--- Components/MenuContent.tsx
+++ Components/MenuContent.tsx
@@ -3,16 +3,16 @@ import React from 'react'
 
 const MenuContent = () => {
     return (
-        <div className='flex md:w-1/2 w-[94%] py-11 justify-center '>
-            <div className="flex w-[394px] h-[307px] p-2">
-                <div className="flex h-full w-full MenuContent_img1 rounded-xl m-2">   
+        <div className='flex md:w-1/2 w-[94%] py-11 justify-center items-center'>
+            <div className="flex w-[394px] h-[307px]">
+                <div className="flex h-full w-full MenuContent_img1 rounded-xl mr-2">   
 
                 </div>
                 <div className="flex w-full h-full flex-col">
-                    <div className="flex w-full h-full MenuContent_img2 rounded-xl m-2 ">
+                    <div className="flex w-full h-full MenuContent_img2 rounded-xl mb-1">
 
                     </div>
-                    <div className="flex w-full h-full MenuContent_img3 rounded-xl">
+                    <div className="flex w-full h-1/2 MenuContent_img3 rounded-xl mt-1">
 
                     </div>
                 </div>
