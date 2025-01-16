cfb3bec3bec3fe3e4f8bb3dbb71d396310f5858c
ahmedkh67 <al.monster.games@gmail.com>
2025-01-16 16:10:52 +0300

    fhfhf

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import {Navbar } from "@/Components";
-import { Hero_Dis, Hero_Button } from "@/Constants";
+import { Hero_Dis} from "@/Constants";
 import Link from "next/link";
 import Moresec from '@/Components/Moresec';
 import Image from 'next/image';
