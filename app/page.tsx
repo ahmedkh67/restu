8afb32069653fcfe5965a4a7bf261198f4263510
ahmedkh67 <al.monster.games@gmail.com>
2025-01-22 03:56:16 +0300

    oiooo

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 
-import { Distance2, Distancesec, Footer, Historysec, Mapsec, Menulist, Menusec, Navbar, Numbox, Teamsec } from "@/Components";
+import { Distance2, Distancesec, Footer, Historysec, Mapsec, Menulist, Menusec, Navbar, Teamsec } from "@/Components";
 import Moresec from "@/Components/Moresec";
 import { Hero_title, Hero_Dis, Hero_Button } from "@/Constants";
 import Link from "next/link";
