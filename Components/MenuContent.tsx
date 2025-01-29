4636031b9c63340cc4c55455ab81f39d0fa6fa08
ahmedkh67 <al.monster.games@gmail.com>
2025-01-29 13:16:39 +0300

    ghg

--- Components/MenuContent.tsx
+++ Components/MenuContent.tsx
@@ -1,20 +1,39 @@
+"use client"
+
 import React from 'react'
+import { motion } from "framer-motion"; 
+
 // height={207} width={394}
 
 const MenuContent = () => {
     return (
         <div className='flex md:w-1/2 w-[94%] py-11 justify-center items-center'>
             <div className="flex w-[394px] h-[307px]">
+                <motion.div className='w-full mr-2'
+                                        initial={{ opacity: 1, scale: 0, rotate: 25}}
+                                        whileInView={{ opacity: 1, scale: 1, rotate: 0}}
+                    
+                                        >
                 <div className="flex h-full w-full MenuContent_img1 rounded-xl mr-2">   
 
-                </div>
+                </div></motion.div>
                 <div className="flex w-full h-full flex-col">
+                <motion.div className='w-full h-full mb-1'
+                                        initial={{ opacity: 1, scale: 0, rotate: -25}}
+                                        whileInView={{ opacity: 1, scale: 1, rotate: 0}}
+                    
+                                        >
                     <div className="flex w-full h-full MenuContent_img2 rounded-xl mb-1">
 
-                    </div>
-                    <div className="flex w-full h-1/2 MenuContent_img3 rounded-xl mt-1">
+                    </div></motion.div>
+                    <motion.div className='w-full h-1/2 mt-1'
+                                        initial={{ opacity: 1, scale: 0}}
+                                        whileInView={{ opacity: 1, scale: 1}}
+                    
+                                        >
+                    <div className="flex w-full h-full MenuContent_img3 rounded-xl mt-1">
 
-                    </div>
+                    </div></motion.div>
                 </div>
             </div>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 "use client "
+
+
 import { Commentssec, Distance2, Distancesec, Footer, Mapsec, Menulist, Menusec, Navbar, Teamsec } from "@/Components";
 import Moresec from "@/Components/Moresec";
 import { Hero_title, Hero_Dis, Hero_Button } from "@/Constants";
@@ -14,16 +16,16 @@ export default function Home() {
             <Navbar />
             <div className="flex w-full h-full flex-col justify-evenly items-center">
                  
-                    <h1 className="md:text-[170px] text-[60px] font-bold text-white">
+                    <h1 className="md:text-[170px] text-[60px] font-bold text-white upanimate">
                         {Hero_title}
                     </h1>
             
-                <h1 className="text-center text-gray-300 md:w-[30%] w-[65%] my-2 pb-6">
+                <h1 className="text-center text-gray-300 md:w-[30%] w-[65%] my-2 pb-6 upanimate2">
                     {Hero_Dis}
                 </h1>
                 
                 <Link href={"#moresec"}>
-                <button className="Button1 md:text-3xl text-4xl px-2 py-0.5 mt-[-7px]">
+                <button className="Button1 md:text-3xl text-4xl px-2 py-0.5 mt-[-7px] upanimate3">
                     {Hero_Button}
                 </button>
                 </Link>
