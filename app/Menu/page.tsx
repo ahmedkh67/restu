57a46dc928c69ead6daccf02d3216d03fe029271
ahmedkh67 <al.monster.games@gmail.com>
2025-02-09 00:23:13 +0300

    nj

--- app/Menu/page.tsx
+++ app/Menu/page.tsx
@@ -33,7 +33,14 @@ const page = () => {
     </div>
 
     <MenuSlider />
-    
+    <div className="flex w-full md:p-6 p-2 pb-6 bg-black justify-center overflow-hidden">
+      <button className='bg-white text-black w-full py-2 pointer  '>
+        <p className='text-center md:text-6xl text-3xl font-bold pointer'>
+          Gallery
+        </p>
+      
+      </button>
+    </div>
 
     <div className="flex w-full justify-center flex-col items-center">
       
@@ -43,6 +50,26 @@ const page = () => {
     <h1 className='md:text-3xl text-xl text-center w-3/4 md:w-1/2'>
       a honored resturant that bulit on passion , love , and consisty , 
     </h1>
+  </div>
+  <div className="flex w-full justify-center items-center pt-10 md:flex-row flex-col">
+    <div className="flex w-full h-40 m-10 rounded-2xl bg-red-300 p-3">
+      <div className="flex w-full h-full rounded-2xl border-white border-2 border-solid 
+      justify-center items-center text-2xl font-bold text-white ">
+        Lunch Menu
+      </div>
+    </div>
+    <div className="flex w-full h-40 m-10 rounded-2xl bg-blue-300 p-3">
+      <div className="flex w-full h-full rounded-2xl border-white border-2 border-solid 
+        justify-center items-center text-2xl font-bold text-white ">
+          Dinner Menu
+        </div>
+      </div>
+    <div className="flex w-full h-40 m-10 rounded-2xl bg-amber-300 p-3">
+      <div className="flex w-full h-full rounded-2xl border-white border-2 border-solid 
+        justify-center items-center text-2xl font-bold text-white ">
+          Sweets Menu
+        </div>
+    </div>
   </div>
   <div className="flex w-full md:p-10 h-full flex-col justify-evenly bg-white mt-10">
     <h1 className="Opacity_animate text-4xl md:text-6xl text-center m-8 ">
@@ -80,7 +107,6 @@ const page = () => {
   <Mapsec />
   <Footer />
   
-
 </>
   )
 }
