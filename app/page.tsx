973a8729f8376627dcd144231509acf1f4efc04c
ahmedkh67 <al.monster.games@gmail.com>
2025-03-03 16:37:45 +0300

    er

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ import Image from "next/image";
 import HomeIcon from '@/public/homeIcon.svg'
 import AboutIcon from '@/public/aboutIcon.svg'
 import MenuIconn from '@/public/menu3Icon.svg'
+import ReserveIcon from '@/public/write-svgrepo-com (3).svg'
 
 
 export default function Home() {
@@ -117,10 +118,23 @@ export default function Home() {
         <Commentssec />
         <Teamsec />
         <div className="flex md:hidden bg-white w-full py-6 px-6 absolute left-0 bottom-0 z-10 justify-evenly items-center fixed ">
-            <Image src={HomeIcon} alt={"home icon "} width={30} height={30} />
-            <Image src={AboutIcon} alt={"home icon "} width={35} height={30} />
-            <Image src={MenuIconn} alt={"home icon "} width={35} height={30} />
-            <Image src={MenuIconn} alt={"home icon "} width={30} height={30} />
+            <Link href={'././'}>
+                <Image src={HomeIcon} alt={"home icon "} width={30} height={30} />
+            </Link>
+            <Link href={'/History'}>
+                <Image src={AboutIcon} alt={"home icon "} width={35} height={30} />
+            </Link>
+            <Link href={'/Menu'}>
+               <Image src={MenuIconn} alt={"home icon "} width={35} height={30} />
+            </Link>
+            <Link href={'/Reservation'}>
+               <Image src={ReserveIcon} alt={"home icon "} width={35} height={30} />
+            </Link>
+
+            
+            
+           
+            
         </div>
         </>
     )
