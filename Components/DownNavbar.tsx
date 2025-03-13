a54343fe2a241eacb98716332d5240085a2271fc
ahmedkh67 <al.monster.games@gmail.com>
2025-03-13 04:24:05 +0300

    vv

--- Components/DownNavbar.tsx
+++ Components/DownNavbar.tsx
@@ -9,10 +9,10 @@ import ReserveIcon from '@/public/write-svgrepo-com (3).svg'
 
 const DownNavbar = () => {
   return (
-    <div className="flex md:hidden bg-white w-full py-3 px-6 absolute left-0 bottom-0 z-10 justify-evenly items-center fixed ">
+    <div className="flex md:hidden bg-white w-full py-1 px-6 absolute left-0 bottom-0 z-10 justify-evenly items-center fixed ">
         <Link href={'././'}>
         <div className="flex flex-col justify-between items-center">
-        <Image src={HomeIcon} alt={"home icon "} width={35} height={30} />
+        <Image src={HomeIcon} alt={"home icon "} width={31} height={30} />
         <p className='text-[10px]'>Home</p>
         </div>
             
@@ -20,21 +20,21 @@ const DownNavbar = () => {
         
         <Link href={'/History'}>
         <div className="flex flex-col justify-between items-center">
-        <Image src={AboutIcon} alt={"home icon "} width={35} height={30} />
+        <Image src={AboutIcon} alt={"home icon "} width={31} height={30} />
         <p className='text-[10px]'>About</p>
         </div>
             
         </Link>
         <Link href={'/Menu'}>
         <div className="flex flex-col justify-between items-center">
-            <Image src={MenuIconn} alt={"home icon "} width={35} height={30} />
+            <Image src={MenuIconn} alt={"home icon "} width={31} height={30} />
             <p className='text-[10px]'>Menu</p>
         </div>
 
         </Link>
         <Link href={'/Reservation'}>
         <div className="flex flex-col justify-between items-center">
-            <Image src={ReserveIcon} alt={"home icon "} width={35} height={30} />                
+            <Image src={ReserveIcon} alt={"home icon "} width={31} height={30} />                
             <p className='text-[10px]'>Reservation</p>
         </div>
         
